f03f91fbf6a410f77757dca6f0cec09c9bba8dbb
kerusks <kerusks@gmail.com>
2020-03-03 09:41:17 +0000

    added sass support

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="wrap">
-    Carousel Here
+    <div class="header-text-container">
+        <h1>Carousel Test</h1>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Carousel"
- };
+};
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.wrap {
+    max-width: 1024px;
+    margin: 0 auto;
+}
+
+</style>
