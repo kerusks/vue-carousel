c5cf50d076507e19fdf5ee4d0c45533346b9335a
kerusks <kerusks@gmail.com>
2020-03-03 09:32:35 +0000

    initial skeleton setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Carousel />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Carousel from "./components/carousel";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Carousel
   }
 };
 </script>
 
+
 <style>
+@import url("https://fonts.googleapis.com/css?family=Ropa+Sans&display=swap");
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: "Ropa Sans", Helvetica, Arial, sans-serif;
+  font-size: 16px;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/carousel.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="wrap">
+    Carousel Here
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Carousel"
+ };
+</script>
+
+<style scoped lang="scss"></style>
